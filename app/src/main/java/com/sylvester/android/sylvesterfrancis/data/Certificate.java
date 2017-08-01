981bccba533ae2b30f54c7bccf6af5c9fc95a898
Sylvester Ranjith Francis <whitefire00700@gmail.com>
2017-08-01 23:35:00 +0530

    added getter and setter for certificates

--- app/src/main/java/com/sylvester/android/sylvesterfrancis/data/Certificate.java
+++ app/src/main/java/com/sylvester/android/sylvesterfrancis/data/Certificate.java
@@ -5,4 +5,41 @@ package com.sylvester.android.sylvesterfrancis.data;
  */
 
 public class Certificate {
+    private String certificate_title, certificate_authority;
+    private int certificate_id;
+
+    public Certificate() {}
+
+
+    public Certificate(String title, String authority,int image_id) {
+        this.certificate_title = title;
+        this.certificate_authority = authority;
+        this.certificate_id = image_id;
+    }
+
+
+    public String gettitle() {
+        return certificate_title;
+    }
+
+
+    public void settitle(String title) {
+        this.certificate_title = title;
+    }
+
+    public String getauthority() {
+        return certificate_authority;
+    }
+
+    public void setdescription(String authority) {
+        this.certificate_authority = authority;
+    }
+
+    public int getCertificate_id() {
+        return certificate_id;
+    }
+
+    public void setCertificate_id(int certificate_id) {
+        this.certificate_id = certificate_id;
+    }
 }
